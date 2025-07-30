6b01806e6f80433d4adfb6537e5a3658d6492fca
Oscar Bender-Stone <oscarbenderstone@gmail.com>
2025-07-30 10:02:06 -0600

    Fixed syntax highlighting with fork of grammar

---
+++ languages/wikitext/highlights.scm
@@ -0,0 +1,15 @@
+(comment) @comment
+
+(h1) @text.title
+; (wikitarget) @link_uri
+; (external_link) @link_uri
+; (wikilabel) @link_text
+
+[
+    (bl1)
+    (bl2)
+    (bl3)
+    (nl1)
+    (nl2)
+    (nl3)
+] @punctuation.list_marker
